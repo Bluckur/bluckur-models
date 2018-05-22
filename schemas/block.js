27bab571786da213f76fe2a3baf1f134f63e2ffb
Vidovitsch <david.de.bekker@gmail.com>
2018-05-22 12:46:02 +0200

    Transaction in block fix

--- schemas/block.js
+++ schemas/block.js
@@ -1,6 +1,9 @@
 const Transaction = require('./transaction');
 const BlockHeader = require('./blockHeader');
 
+const TransactionItem = { type: Transaction };
+
 module.exports = ({
+  transactions: [TransactionItem],
   blockHeader: BlockHeader,
 });
