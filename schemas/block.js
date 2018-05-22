b3a01b6f6fc4873cc85eefc120a27cdd91fa3500
Vidovitsch <david.de.bekker@gmail.com>
2018-05-22 13:33:54 +0200

    Test

--- schemas/block.js
+++ schemas/block.js
@@ -2,6 +2,6 @@ const Transaction = require('./transaction');
 const BlockHeader = require('./blockHeader');
 
 module.exports = ({
-  transactions: { type: Array, arrayType: Transaction },
+  transactions: Transaction,
   blockHeader: BlockHeader,
 });
